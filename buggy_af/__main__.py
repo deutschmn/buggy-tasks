730f30b25a6729af7d2fec283faa73d57647f3a8
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2025-03-22 15:46:52 +0100

    Add generated version of streamlit app

---
+++ buggy_af/__main__.py
@@ -0,0 +1,9 @@
+import sys
+
+from streamlit.web import cli as stcli
+
+if __name__ == "__main__":
+    from buggy_af import app
+
+    sys.argv = ["streamlit", "run", app.__file__]
+    sys.exit(stcli.main())
