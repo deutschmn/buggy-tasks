6aab3c1b717a51236ed034cf8424ef4d9fe4cee1
Patrick Deutschmann <patrick.deutschmann@dedalus.com>
2025-05-12 16:09:11 +0200

    new init

--- buggy_tasks/commands/__init__.py
+++ buggy_tasks/commands/__init__.py
@@ -1,1 +1,11 @@
+"""
+Command System Package
+
+This package provides the slash command functionality for the Buggy Tasks application.
+"""
+
+# Re-export the command registry and processing function
 from buggy_tasks.commands.registry import process_command, registry
+
+# Version information
+__version__ = '0.1.0'
