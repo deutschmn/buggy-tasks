860267bebc263d71d6e182834fd8fdf221244ace
Patrick Deutschmann <patrick.deutschmann@dedalus.com>
2025-05-12 16:02:47 +0200

    Modfiy init

--- buggy_tasks/__init__.py
+++ buggy_tasks/__init__.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
-Buggy Tasks - A simple todo list application
+Buggy Tasks - A Streamlit-based Todo List Application
+
+This package provides a simple but powerful todo list application
+with automatic tagging, priority calculation, and command processing.
+
+Features:
+- Automatic tag generation using Mistral AI
+- Priority calculation based on tags
+- Slash commands support
+- Persistent storage of todos
 """
 
 __version__ = "0.1.0"
+__author__ = "Patrick Deutschmann"
